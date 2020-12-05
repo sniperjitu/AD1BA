d0c6f5dd0aff6bc6e78a56265f54f9b518a5c739
sniperjitu <75540707+sniperjitu@users.noreply.github.com>
2020-12-06 01:48:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'NAMA BOTMU'; 
-const instagram = 'LINK INSTAGRAMU'; 
-const telegram = 'LINK TELEMU'; 
-const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'LINK YTMU';
+const BotName = 'AD 1 BA'; 
+const instagram = 'https://www.instagram.com/npcdl/'; 
+const telegram = 'https://t.me/pongo_joe'; 
+const kapanbotaktif = 'SEKAREP MU BOSS'; 
+const youtube = 'ORA DUE YTB';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
